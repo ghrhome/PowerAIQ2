6aee0d296fbf70f50befd54c3b96e92fd024d31a
Jiankang.ljk <jiankang.ljk@alipay.com>
2017-06-17 19:45:32 +0800

    add new mlp model

---
+++ powerai_intention_mlp.py
@@ -0,0 +1,76 @@
+'''Trains and evaluate a simple MLP
+on the Reuters newswire topic classification task.
+'''
+from __future__ import print_function
+
+import numpy as np
+import keras
+import powerai_data
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
+import sys
+
+max_words = 512
+batch_size = 32
+epochs = 45
+
+train_intention = True
+if len(sys.argv) > 1:
+    if sys.argv[1] == "a":
+        train_intention = False
+
+
+print('Loading data...')
+(x_a_train, x_b_train, y_cate_train, y_inte_train), (
+    x_a_test, x_b_test, y_cate_test, y_inte_test) = powerai_data.load_data()
+
+print("Train Intention")
+(x_train, y_train), (x_test, y_test) = (x_b_train,
+                                        y_inte_train), (x_b_test, y_inte_test)
+
+
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+num_classes = np.max(y_train) + 1
+print(num_classes, 'classes')
+
+print('Vectorizing sequence data...')
+tokenizer = Tokenizer(num_words=max_words)
+x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
+x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+print('Convert class vector to binary class matrix '
+      '(for use with categorical_crossentropy)')
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+print('Building model...')
+model = Sequential()
+model.add(Dense(256, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes * 2))
+model.add(Dense(128, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test), validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
