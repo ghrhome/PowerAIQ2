8ee459aa7a2aee11234253d949c43c1caad50e38
WangYingbo <superwyb@gmail.com>
2017-06-17 17:28:38 +0800

    fix bug

--- run_predict.py
+++ run_predict.py
@@ -9,7 +9,9 @@ from keras.preprocessing.text import Tokenizer
 import sys
 from keras.models import load_model
 import pickle
+import run_predict_data
 
+batch_size=10
 # load model
 product_model = load_model("model_product_7.h5")
 intention_model = load_model("model_intention_7.h5")
@@ -19,6 +21,12 @@ word_dict = pickle.load(open('word_dict.bin', "rb"))
 category_dict = pickle.load(open('category_dict.bin', "rb"))
 intention_dict = pickle.load(open('intention_dict.bin', "rb"))
 
-print(word_dict)
-print(category_dict)
-print(intention_dict)
+(id_list, test_a, test_b) = run_predict_data.load_data("./trainval/data/", word_dict)
+
+print(id_list)
+print(test_a)
+print(test_b)
+result_a = product_model.predict(test_a, batch_size=batch_size, verbose=1)
+result_b = intention_model.predict(test_b, batch_size=batch_size, verbose=1)
+
+
--- run_predict_data.py
+++ run_predict_data.py
@@ -3,18 +3,22 @@
 
 from os import listdir
 import pickle
-def load_data(dir_path):
+import jieba
+import numpy as np
+
+
+def load_data(dir_path, word_dict):
     data_files = listdir(dir_path)
-
-
-    train_items = []
+    train_items_a = []
+    train_items_b = []
+    id_list = []
     for data_file in data_files:
         # get id based on file name
         txt_id = data_file.split(".")[0]
         # handle data file
-        f = open(data_dir_path + data_file, "rb")
+        f = open(dir_path + data_file, "rb")
         lines = f.readlines()
-        f.close()
+
         speaker_a = []
         speaker_b = []
         a_speaking = True
@@ -39,3 +43,21 @@ def load_data(dir_path):
                     speaker_a.append(l)
                 else:
                     speaker_a.append(l)
+        f.close()
+        id_list.append(txt_id)
+        train_items_a.append(convert_speaker_list_to_seg_id_list(speaker_a, word_dict))
+        train_items_b.append(convert_speaker_list_to_seg_id_list(speaker_b, word_dict))
+
+    return np.array(id_list), np.array(train_items_a), np.array(train_items_b)
+
+
+def convert_speaker_list_to_seg_id_list(speaker_list, word_dict):
+    seg_id_list = []
+    for line in speaker_list:
+        seg_list = jieba.cut(line)
+        for seg in seg_list:
+            if word_dict[seg]:
+                seg_id_list.append(word_dict[seg]["id"])
+            else:
+                print("Warning. Word not found." + seg)
+    return seg_id_list
